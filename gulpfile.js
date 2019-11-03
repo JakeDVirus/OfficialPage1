3a8d7b7f4537aba57fc4dd5e89323550ee3d4690
JakeDVirus <neelunstp@gmail.com>
2019-11-03 14:32:22 +0530

    newNode: phase 3: incorporate gulp and initialize with sass

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+//include gulp and plugins
+let gulp = require('gulp');
+
+//file location
+let
+    source = 'source/',
+    dest = 'build/';
+
+//default task
+gulp.task('default', function(){
+
+});
