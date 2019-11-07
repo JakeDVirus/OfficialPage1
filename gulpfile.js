f54c31e0a1352c3a3e518d5779c5160054fcda39
JakeDVirus <neelunstp@gmail.com>
2019-11-07 14:33:22 +0530

    node: phase 3: gulp..

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,68 @@
-//include gulp and plugins
-let gulp = require('gulp');
 
-//file location
-let
-    source = 'source/',
-    dest = 'build/';
+/*#########################################
+####   include gulp and plugins       #####
+##########################################*/
+const gulp = require('gulp'),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    postcss = require('gulp-postcss'),
+    replace = require('gulp-replace'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify');
 
-//default task
-gulp.task('default', function(){
 
-});
+/*#########################################
+####          file location           #####
+##########################################*/
+const
+    source = 'src/',
+    dest = 'build/',
+    
+    css = {
+        in: source + 'sass/styles.scss',
+        watchFiles: source + 'sass/**/*',
+        out: dest + 'css/',
+        // sassOpts: {
+        //     outputStyle: 'nested',
+        //     errLogToConsole: true
+        // }
+    };
+
+
+
+/*#########################################
+####              tasks               #####
+##########################################*/
+
+//Sass task
+function sassTask() {
+    return gulp.src(css.in)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([ autoprefixer(), cssnano() ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(css.out))
+}
+
+//watch task
+function watch() {
+    gulp.watch(css.watchFiles, sassTask); 
+}
+
+
+/*#########################################
+####            export tasks          #####
+##########################################*/
+
+exports.sass = sassTask;
+
+
+
+/*#########################################
+####            default tasks          #####
+##########################################*/
+
+gulp.task('default', gulp.parallel(watch));
