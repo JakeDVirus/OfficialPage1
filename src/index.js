c722181c469ecd1b9f4a2c42fc0e52707eb65818
Jake Giri <neelunstp@gmail.com>
2019-12-06 17:11:15 +0530

    webpack: succeed: 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'animate.css/source/_base.css';
+import 'animate.css/source/sliding_entrances/slideInRight.css';
+
 import './sass/styles.scss';
 import 'waypoints/lib/noframework.waypoints.min.js';
 import * as myAnimationOnScroll from './js/organisedPartials/animationOnScrollPartial';
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,13 @@ module.exports = {
                 ]
             },
             {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
                 test: /\.scss$/,
                 use: [
                     'style-loader',
